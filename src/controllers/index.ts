cd4cdfa21c08663a0d9b828239e9b1934e0d760b
JuhoKon <kontiainen97@hotmail.fi>
2020-12-21 13:46:08 +0200

    google maps api

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,23 @@
 import { Request, Response } from "express";
+import axios from "axios";
+const API = "AIzaSyACED-iW3pMSvqLlQrgx4TQ5cZEy7DrY64";
 
-/**
- * GET /
- * Home page.
- */
 export const index = async (req: Request, res: Response): Promise<void> => {
-    res.render("index", { title: "Express" });
+  res.json({ ok: "OKEI" });
+};
+
+export const autocomplete = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  const query = req.query.query;
+  if (!query) {
+    res.status(400).json({ error: "No query" });
+    return;
+  }
+  const results = await axios.get(
+    `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${query}&key=${API}`,
+  );
+  const data = results.data;
+  res.json({ data });
 };
